2a47ea035feace95816bcff0bddd9dc1303fd983
REDDYKARTHIKEYASAHITHREDDY <138746870+REDDYKARTHIKEYASAHITHREDDY@users.noreply.github.com>
2024-07-22 00:11:01 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Reddy Karthikeya Sahith Reddy</h1>
+            <p><strong>Contact:</strong> +917993995644 | <a href="mailto:karthikeyasahithreddyreddy99@gmail.com">karthikeyasahithreddyreddy99@gmail.com</a></p>
+            <p><a href="https://www.linkedin.com/in/reddy-karthikeya-sahith-reddy/">LinkedIn</a> | <a href="https://github.com/REDDYKARTHIKEYASAHITHREDDY">GitHub</a></p>
+        </header>
+    );
+};
+
+export default Header;
